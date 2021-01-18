0aa339cfc6901cc7fc4fa4117d3ab21509cf3dc4
zenehu <70635027+zeronethunter@users.noreply.github.com>
2021-01-18 16:03:18 +0300

    Update Queue ( LR7 )
    Second try

--- Queue.cpp
+++ Queue.cpp
@@ -8,58 +8,44 @@ struct Node {
 };
 
 struct Queue {
-    Queue(Node* start, Node* end) {
-        first = start;
-        last = end;
-    }
-    Node* first;
-    Node* last;
+    Node* first = nullptr;
+    Node* last = nullptr;
 };
-Queue& constructor() {
-    Queue queue(nullptr, nullptr);
-    if (queue.first != nullptr) {
-        cout << "Queue constructed!\n";
-    }
-    return queue;
+
+void constructor(Queue& queue) {
+    queue.first = nullptr;
+    queue.last = nullptr;
 }
 
 void push(Queue& queue, const Node& node) {
+    Node* new_element = new Node;
+    new_element->information = node.information;
     if (queue.first == nullptr) {
-        Node* new_element = new Node;
-        new_element->information = node.information;
         queue.first = new_element;
-        queue.last = new_element;
     } else {
-        Node* new_element = new Node;
-        new_element->information = node.information;
         queue.last->next = new_element;
-        queue.last = new_element;
     }
+    queue.last = new_element;
 }
 
 Node& pop(Queue& queue) {
-    Node *result = new Node;
-    Node *node;
-    node = queue.first;
-    result->information = node->information;
+    Node *node = queue.first;
     queue.first = queue.first->next;
-    delete node;
-    return *result;
+    return *node;
 }
 
 void print (const Queue& queue) {
     cout << "Your queue:" << endl;
     Node* node = queue.first;
-    if (queue.first != queue.last) {
+    if (queue.first != nullptr) {
         while (node != queue.last) {
             cout << node->information << endl;
             node = node->next;
         }
         cout << node->information << endl;
-    } else if (queue.first != queue.last){
-        cout << node->information << endl;
-    } else
+    } else {
         cout << "Queue doesn't exist" << endl;
+    }
 }
 
 unsigned int size(Queue& queue) {
@@ -74,21 +60,22 @@ unsigned int size(Queue& queue) {
 }
 
 void destructor(Queue& queue) {
-    Node *trash = queue.first;
+    Node* trash = queue.first;
     if (queue.first != queue.last) {
         while (queue.first != queue.last) {
             queue.first = queue.first->next;
             delete trash;
             trash = queue.first;
         }
+        delete trash;
     }
-    trash = queue.first;
-    queue = constructor();
-    delete trash;
+    queue.first = nullptr;
+    queue.last = queue.first;
 }
 
 int main() {
-    Queue queue = constructor();
+    Queue queue;
+    constructor(queue);
 
     for (int i = 0; i <= 12; ++i) {
         Node node;
@@ -107,6 +94,8 @@ int main() {
     cout << "Size of queue is " << size(queue) << endl;
 
     destructor(queue);
+    print(queue);
+    push(queue, node);
     print(queue);
 
 
